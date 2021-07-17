d64978919238b93fc8776a46b07a79a53454faf0
OZIOstatMAn <oziomakenechukwu7@gmail.com>
2021-07-17 03:51:58 +0100

    Add files via upload

---
+++ MyFileReader.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.util.Formatter;
+
+class AnExtension {
+    public static void theFile() {
+        File x = new File("C:\\Users\\Stentechy\\OziFileReader.txt");
+        if(x.exists()) {
+            System.out.println(x.getName() + " exists! because Ozioma created one :wink:");
+        }
+        else {
+            System.out.println("The file does not exist...");
+            try {
+                Formatter F = new Formatter("C:\\Users\\Stentechy\\OziFileReader.txt");
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            if(x.exists()) {
+                System.out.println(x.getName() + " now exists, thanks to Ozioma. Check the folder! :wink:");
+            }
+        }
+    }
+    public static void writeToFile() {
+        try {
+            Formatter D = new Formatter("C:\\Users\\Stentechy\\OziFileReader.txt");
+            D.format("%s %s", "1.", "OZIOMA CREATED THIS USING JAVA");
+            D.format("\n%s %s", "2.", "Whatever you do in life, fuck sometimes!");
+            D.close();
+            System.out.println("You've successfully created and written to a file, BABY!!!");
+        } catch (Exception e) {
+            System.out.println("Cannot write to file");
+        }
+
+    }
+}
+
+public class MyFileReader extends AnExtension{
+    public static void main(String[] args) {
+        theFile();
+        writeToFile();
+    }
+}
